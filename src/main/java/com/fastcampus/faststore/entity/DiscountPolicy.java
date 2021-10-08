bc070e862e10bbd8afdfb7a13d71e0292b80366e
Heonjun Jeong <gjswns1991@gmail.com>
2021-10-08 18:49:19 +0900

    서점 도서 관리 및 판매

---
+++ src/main/java/com/fastcampus/faststore/entity/DiscountPolicy.java
@@ -0,0 +1,33 @@
+package com.fastcampus.faststore.entity;
+
+import com.fastcampus.faststore.type.DiscountType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+/**
+ * 할인 정책 Entity
+ * 할인 타입과 액수를 가진다
+ */
+public class DiscountPolicy extends BaseEntity {
+
+    private DiscountType discountType;
+
+    private Long amount;
+
+    // TODO: discountType에 따라 price에서 차액을 빼거나, 퍼센트 할인 가격을 반환하는 메서드를 완성하시오. DiscountPolicyTest를 성공시켜야 합니다.
+    public Long getDiscountAmount(Long price) {
+        if(this.getDiscountType()==DiscountType.AMOUNT)
+            return price- amount;
+        else
+            return price-(price/amount);
+    }
+}
---
+++ src/test/java/com/fastcampus/faststore/service/BookServiceTest.java
@@ -0,0 +1,56 @@
+package com.fastcampus.faststore.service;
+
+import com.fastcampus.faststore.entity.Book;
+import com.fastcampus.faststore.entity.BookSale;
+import com.fastcampus.faststore.entity.DiscountPolicy;
+import com.fastcampus.faststore.repository.BookRepository;
+import com.fastcampus.faststore.type.DiscountType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+public class BookServiceTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    private BookService bookService;
+
+    @BeforeEach
+    public void setup() {
+        bookService = new BookService(bookRepository);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        bookRepository.deleteAll();
+    }
+
+    // TODO: getOrThrow 테스트 코드를 작성하세요.
+    @Test
+    public void getOrThrow() {
+        Book book = new Book("자바의 정석", "남궁성", 30000L);
+        bookRepository.save(book);
+
+        Book result = bookService.getOrThrow("자바의 정석");
+
+        assertThat(result).isNotNull();
+        assertThat(result.getTitle()).isEqualTo(book.getTitle());
+        assertThat(result.getAuthor()).isEqualTo(book.getAuthor());
+        assertThat(result.getPrice()).isEqualTo(book.getPrice());
+
+    }
+
+    @Test
+    public void registerBook() {
+        bookService.registerBook("자바의 정석", "남궁성", 3000L);
+        assertThat(bookRepository.findByTitle("자바의 정석")).isNotNull();
+
+    }
+}
